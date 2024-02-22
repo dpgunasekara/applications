90fb547403d7e3ced5d7f3b2d9a25743ad2ce2bf
Novustech-SL <110025134+Novustech-SL@users.noreply.github.com>
2024-02-22 12:23:41 +0800

    Update create-disk.ps1

--- uat-platform/create-disk.ps1
+++ uat-platform/create-disk.ps1
@@ -1,1 +1,2 @@
 az disk create --resource-group rg-dcrs-uat-01 --name keycloak-postgres-data-01 --size-gb 20 --query id --output tsv
+az disk create --resource-group rg-dcrs-uat-01 --name novatrackKeycloak-postgres-data-01 --size-gb 20 --query id --output tsv
