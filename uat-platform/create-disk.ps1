4035a5adbb33c585ab12734f61bc89e927ace893
robvde <robvde@novustech.com.sg>
2024-05-29 10:39:52 +0000

    novatrack adjustments

--- uat-platform/create-disk.ps1
+++ uat-platform/create-disk.ps1
@@ -1,5 +1,4 @@
 az disk create --resource-group rg-dcrs-uat-01 --name keycloak-postgres-data-01 --size-gb 20 --query id --output tsv
-az disk create --resource-group rg-dcrs-uat-01 --name novatrackkeycloak-postgres-data-01 --size-gb 20 --query id --output tsv
 az disk create --resource-group rg-dcrs-uat-01 --name zookeeper-data-0 --size-gb 5 --query id --output tsv
 az disk create --resource-group rg-dcrs-uat-01 --name zookeeper-data-1 --size-gb 5 --query id --output tsv
 az disk create --resource-group rg-dcrs-uat-01 --name zookeeper-data-2 --size-gb 5 --query id --output tsv
