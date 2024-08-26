ba0baead5100ed0f3b17c2eb9434211f64b7750a
robvde <robvde@novustech.com.sg>
2024-08-26 13:10:33 +0000

    cert updates

--- uat-platform/updatecerts.ps1
+++ uat-platform/updatecerts.ps1
@@ -1,30 +1,34 @@
 # List of services
 $services = @(
-    "bobagency-service",
-    "bobaudit-service",
-    "bobauditservice-service",
-    "bobburs-service",
-    "bobfileprocessing-service",
-    "boblogger-service",
-    "bobmessaging-service",
-    "bobmpos-service",
-    "bobmposinterface-service",
-    "bobmposlogger-service",
-    "boboag-service",
-    "bobonegov-service",
-    "bobpayment-service",
-    "bobpaymentapi-service",
-    "bobqueuemanager-service",
-    "bobreferencing-service",
-    "bobreporting-service",
-    "bobswitchinterface-service",
-    "boburm-service",
-    "bobuserlimits-service"
+    "bobagency",
+    "bobaudit",
+    "bobauditservice",
+    "bobburs",
+    "bobfileprocessing",
+    "boblogger",
+    "bobmessaging",
+    "bobmpos",
+    "bobmposinterface",
+    "bobmposlogger",
+    "boboag",
+    "bobonegov",
+    "bobpayment",
+    "bobpaymentapi",
+    "bobqueuemanager",
+    "bobreferencing",
+    "bobreporting",
+    "bobswitchinterface",
+    "boburm",
+    "bobuserlimits"
 )
 
 # Iterate over each service and execute the kubectl command
 foreach ($service in $services) {
-    $command = "kubectl create secret generic $service-keystore --from-file=$service.p12=.\certificates\$service.p12"
+    $command = "kubectl delete secret $service-keystore"
+    Write-Host "Executing: $command"
+    Invoke-Expression $command
+
+    $command = "kubectl create secret generic $service-keystore --from-file=$service-service.p12=.\certificates\$service-service.p12"
     Write-Host "Executing: $command"
     Invoke-Expression $command
 }
