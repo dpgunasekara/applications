e8a96978ac88948504724023de2e6c62eaf6c219
Rob van der Ent <robvde@novustech.com.sg>
2025-04-13 07:38:36 +0800

    ficed

--- uat/create-folders.ps1
+++ uat/create-folders.ps1
@@ -1,60 +1,42 @@
-STORAGE_ACCOUNT="n0vdrcsuat01"
+$STORAGE_ACCOUNT = "n0vdrcsuat01"
 
-names=(
-  bobagency
-  bobaudit
-  bobburs
-  bobfileprocessing
-  boblogger
-  bobmessaging
-  bobmpos
-  bobmposinterface
-  bobmposlogger
-  boboag
-  bobonegov
-  bobpayment
-  bobqueuemanager
-  bobreferencing
-  bobreporting
-  boburm
-  bobuserlimits
-  bobmerchantmate
-  bobnovatrackintapp
-  bobnovatrackextapp
-  bobcoindeposit
-  bobr3portal
-  bobsignage
-  bobnovatrackintportal
-  bobnovatrackextportal
-  bobburssimulator
+$names = @(
+  "bobagency"
+  "bobaudit"
+  "bobburs"
+  "bobfileprocessing"
+  "boblogger"
+  "bobmessaging"
+  "bobmpos"
+  "bobmposinterface"
+  "bobmposlogger"
+  "boboag"
+  "bobonegov"
+  "bobpayment"
+  "bobqueuemanager"
+  "bobreferencing"
+  "bobreporting"
+  "boburm"
+  "bobuserlimits"
+  "bobmerchantmate"
+  "bobnovatrackintapp"
+  "bobnovatrackextapp"
+  "bobcoindeposit"
+  "bobr3portal"
+  "bobsignage"
+  "bobnovatrackintportal"
+  "bobnovatrackextportal"
+  "bobburssimulator"
 )
 
-SHARE_NAME="config"
+$shareNames = @("config", "data", "log")
 
-for name in "${names[@]}"; do
-  echo "Creating folder: $name"
-  az storage directory create \
-    --account-name "$STORAGE_ACCOUNT" \
-    --share-name "$SHARE_NAME" \
-    --name "$name"
-done
-
-SHARE_NAME="data"
-
-for name in "${names[@]}"; do
-  echo "Creating folder: $name"
-  az storage directory create \
-    --account-name "$STORAGE_ACCOUNT" \
-    --share-name "$SHARE_NAME" \
-    --name "$name"
-done
-
-SHARE_NAME="log"
-
-for name in "${names[@]}"; do
-  echo "Creating folder: $name"
-  az storage directory create \
-    --account-name "$STORAGE_ACCOUNT" \
-    --share-name "$SHARE_NAME" \
-    --name "$name"
-done
+foreach ($SHARE_NAME in $shareNames) {
+  foreach ($name in $names) {
+    Write-Output "Creating folder '$name' in share '$SHARE_NAME'"
+    az storage directory create `
+      --account-name $STORAGE_ACCOUNT `
+      --share-name $SHARE_NAME `
+      --name $name
+  }
+}
