8899b41ab1b976a9d4ebef38b4cec6229445a59e
Robert van der Ent <robvde@novustech.com.sg>
2025-08-31 05:03:18 +0000

    Deleted deploy-zookeeper.ps1

--- prd/deploy-zookeeper.ps1
+++
@@ -1,3 +0,0 @@
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
-helm install zookeeper bitnami/zookeeper --set replicaCount=3 --set auth.enabled=false --set allowAnonymousLogin=true --version 3.9.3 -n prd
