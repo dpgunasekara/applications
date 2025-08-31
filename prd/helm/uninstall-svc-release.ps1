865a723274c9b618f9e16f64856d61d28a8337c6
Robert van der Ent <robvde@novustech.com.sg>
2025-08-31 12:56:17 +0000

    Updated uninstall-svc-release.ps1

--- prd/helm/uninstall-svc-release.ps1
+++ prd/helm/uninstall-svc-release.ps1
@@ -1,8 +1,2 @@
-# Navigate to the Helm chart directory
-Set-Location "$env:USERPROFILE\Documents\NovusTechComSg\BOB-DRCS\bobcicd\uat\helm\novusware-containers"
-
 # Uninstall the Helm release
-helm uninstall svc-release --namespace drcs-uat-01
-
-# Navigate back to the previous directory
-Set-Location "$env:USERPROFILE\Documents\NovusTechComSg\BOB-DRCS\bobcicd\uat\helm"
+helm uninstall svc-release --namespace prd
