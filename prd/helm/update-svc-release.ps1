9a953da16d94a2358e9f3072f582977b3efd5ffe
Robert van der Ent <robvde@novustech.com.sg>
2025-08-31 12:56:40 +0000

    Updated update-svc-release.ps1

--- prd/helm/update-svc-release.ps1
+++ prd/helm/update-svc-release.ps1
@@ -1,8 +1,2 @@
-# Navigate to the Helm chart directory
-Set-Location "$env:USERPROFILE\Documents\NovusTechComSg\BOB-DRCS\bobcicd\uat\helm\novusware-containers"
-
 # Upgrade the Helm release
-helm upgrade svc-release . --namespace drcs-uat-01
-
-# Navigate back to the previous directory
-Set-Location "$env:USERPROFILE\Documents\NovusTechComSg\BOB-DRCS\bobcicd\uat\helm"
+helm upgrade svc-release . --namespace prd
